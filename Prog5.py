db45ccc5776d5250c73fb8f9b7cddc1e7fc42b76
EyemMateo <eyemmateo@gmail.com>
2026-03-02 23:49:17 -0600

    prog5.4

--- Prog5.py
+++ Prog5.py
@@ -18,9 +18,10 @@ def sumOfSquares(intValue):
         i = i + 1
     return summ
 def numberOfBuses(numOfStudents):
-    i = int(0)
     stuL = numOfStudents
     bus = int(1)
+    if numOfStudents == 0:
+        return(0)
     while stuL > 30:
         bus = bus + 1
         stuL = stuL - 30
@@ -45,21 +46,28 @@ def compareTo(intValue1, intValue2):
         return(1)
     elif intValue1 == intValue2:
         return(0)
-def closeEnough(intValue1,intValue2, tolerance):
+def closeEnough(intValue1, intValue2, tolerance):
     if (abs(intValue1 - intValue2,)) <= tolerance:
         return(True)
     else:
         return(False)
 def daysInFebruary(year):
+    if (year % 100) == 0:
+        if (year % 400) == 0:
+            return(29)
     if (year % 4) == 0:
         day = 29
     else:
         day = 28
     return(day)
-def daysInMonth(month,year):
+
+def daysInMonth(month, year):
     if month == 2:
-        daysInFebruary(year)
+        day = daysInFebruary(year)
+        return(day)
     elif month == 1 or month == 3 or month == 5 or month == 7 or month == 8 or month == 10 or month == 12:
         return(31)
     else:
         return(30)
+    
+print(daysInMonth(2,2023))
