4d451510ca7c77bf154368399b9402f2fa3fa08f
EyemMateo <eyemmateo@gmail.com>
2026-03-02 23:40:40 -0600

    prog5.3

--- Prog5.py
+++ Prog5.py
@@ -21,7 +21,7 @@ def numberOfBuses(numOfStudents):
     i = int(0)
     stuL = numOfStudents
     bus = int(1)
-    while stuL >= 30:
+    while stuL > 30:
         bus = bus + 1
         stuL = stuL - 30
     return bus
@@ -45,6 +45,21 @@ def compareTo(intValue1, intValue2):
         return(1)
     elif intValue1 == intValue2:
         return(0)
-
-#print(square(int(input("Square input"))))
-#print(compareTo((int(input("SquareSum input"))), (int(input("SquareSum input")))))
+def closeEnough(intValue1,intValue2, tolerance):
+    if (abs(intValue1 - intValue2,)) <= tolerance:
+        return(True)
+    else:
+        return(False)
+def daysInFebruary(year):
+    if (year % 4) == 0:
+        day = 29
+    else:
+        day = 28
+    return(day)
+def daysInMonth(month,year):
+    if month == 2:
+        daysInFebruary(year)
+    elif month == 1 or month == 3 or month == 5 or month == 7 or month == 8 or month == 10 or month == 12:
+        return(31)
+    else:
+        return(30)
